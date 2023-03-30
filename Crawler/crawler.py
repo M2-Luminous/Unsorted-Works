9b870ae274633af5c80adea08a541baf643e3635
Minjuan Luo <luom@tcd.ie>
2023-03-30 14:52:13 +0800

    uploaded customer review scraper

---
+++ Crawler/crawler.py
@@ -0,0 +1,84 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+import csv
+
+def scrape_car_reviews(manufacturer, model, year):
+    # Replace the spaces and dashes 
+    model = model.replace(" ", "_")                 #.replace("-", "_")
+    model = model.replace("-", "_")
+    manufacturer = manufacturer.replace(" ", "_")                 #.replace("-", "_")
+    manufacturer = manufacturer.replace("-", "_")
+    
+    # Construct the URL 
+    url = f"https://www.cars.com/research/{manufacturer}-{model}-{year}/consumer-reviews/?page_size=200"
+    
+
+
+    # Send a request to the URL and get the response
+    response = requests.get(url)
+    # Status Checking
+    if response.status_code != 200:
+        print(f"Error: Request failed with status code {response.status_code}")
+        return
+    # Parser generated
+    soup = BeautifulSoup(response.text, "html.parser")
+    
+    # Find consumer review containers
+    review_containers = soup.find_all(class_="consumer-review-container")
+    
+    # Dictionaries to store
+    review_data = []
+    
+    for container in review_containers:
+
+        # Find the title of the review
+        title = container.find(class_="review-card-header__title").text.strip()
+        
+        ###########################################################################
+        
+        # Find the author of the review
+        author_date = container.find(class_="review-card-header__byline").text.strip()
+
+        # Find regular expression pattern and search for match string
+        author_regex = re.compile(r"By (.+?) on")
+        author_match = author_regex.search(author_date)
+        author = author_match.group(1)
+
+        ##########################################################################
+
+        # Find the date of the review
+        date_date = container.find(class_="review-card-header__date").text.strip()
+
+        # Find regular expression pattern and search for match string
+        date_regex = re.compile(r"on (.+)$")
+        date_match = date_regex.search(date_date)
+        date = date_match.group(1)
+
+        ##########################################################################
+        
+        # Find the content of the review
+        content = container.find("p").text.strip()
+
+        ##########################################################################
+        
+        # Store data into dictionary
+        review_data.append({
+            "Title": title,
+            "Author": author,
+            "Date": date,
+            "Content": content
+        })
+    
+    # Write the extracted data to a new CSV file
+    with open(f"{manufacturer}_{model}_{year}_reviews.csv", "w", newline="") as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=["Title", "Author", "Date", "Content"])
+        writer.writeheader()
+        writer.writerows(review_data)
+
+        #writer.writerow([title, author, date, content])
+        
+    print(f"Data written to {manufacturer}_{model}_{year}_reviews.csv")
+
+
+#Example usage: scrape_car_reviews("nissan", "gtr", "2015")
